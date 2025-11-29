244abd6445453b44eb2ce168567dbe13d0dede43
FernandoGv <fernnandovilagrand@gmail.com>
2025-11-29 11:58:01 -0300

    Refactor sitemap routes to use extendRoutes function
    Replaces the static 'routes' array with an 'extendRoutes' function for route configuration in sitemap.config.ts. This change may improve flexibility for future route extensions.

--- sitemap.config.ts
+++ sitemap.config.ts
@@ -1,14 +1,17 @@
 export default {
   hostname: "https://superficiepura.com.br",
+
   generateRobotsTxt: true,
 
   robots: [
     { userAgent: "*", allow: "/" },
   ],
 
-  routes: [
-    "/",
-    "/Sobre",
-    "/Galeria",
-  ],
+  extendRoutes() {
+    return [
+      "/",
+      "/Sobre",
+      "/Galeria",
+    ];
+  }
 };
